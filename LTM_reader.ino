2f79aa91ecd2b42ef6fb78455eee482651460414
sppnk <sppnk@users.noreply.github.com>
2017-03-08 14:57:13 +0100

    Add files via upload

---
+++ LTM_reader.ino
@@ -0,0 +1,283 @@
+
+
+// ************************************************************
+//            LTM READER
+//
+// Simple LTM Telemetry parser and displayer for OLED 128x64 SSD1306
+// Based in code from Kipk (Getthostation)and others
+// Mainly used for INAV flight controllers
+// LTM telemetry is designed for low baudrates, only downlink. Its best goal is 
+// the aircraft is sending its GPS position (and other data) continuosly, so it is
+// your 'GPS beacon' to find your lost plane.
+// This is intended only for OLED telemetry, althought can be resized to antennatracker.
+//
+//
+// sppnk 2017
+//
+//
+// ********************************************************
+// NOTES:
+//
+//
+// You can mount this project inside your transmitter, or throught a serial bluetooth connection (easier). This way
+// EZGUI can be used at the same time with BT connection
+
+// Pushbutton optional to change screens .
+//
+// if you use bluetooth, HC-05 must be set to automatic pairing with the transmitter BT module.
+// Connect BT module tx pin to pin8 in arduino
+// if you make the mod in your transmitter instead serial Bluetooth connection, you can connect directly you TX line to pin8
+// be carefull to supply your Arduino through BEC (mine takes BAT+ from Internal pinout for external module)
+// 
+//
+//
+// TODO:
+// - code is crappy, althought it works, a clean it up is neccesary
+// - add buzzer to create vario 
+// - add buzzer to create distance, altitude or RSSI audible alarms
+// - add arrow graphic for bearing home instead value, to fly by instruments if video is lost.
+// - many more TODOs
+// 
+
+#ifdef membug
+#include <MemoryFree.h>
+#endif
+
+#include <Wire.h>               //if OLED is CO-16, short pins 13-14 to use this.
+//#include <Adafruit_GFX.h>     //not used in this code
+#include <Adafruit_SSD1306.h> // memory consumer library. fixme
+
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+#define PROTOCOL_LIGHTTELEMETRY
+#define BUTTON        4
+#define OLED_PANELS   6
+#define LTM_BAUDS     9600        // the lower the better
+
+//#include <SoftwareSerial.h>                   //this Software serial library gives many problmems
+//SoftwareSerial ltmSerial(8, 9);//8-RX, 9-TX
+//Serial ltmserial;
+
+#include <AltSoftSerial.h>
+AltSoftSerial ltmSerial(8, 9);//8-RX, 9-TX
+
+#include "Lighttelemetry.cpp"
+#include <stdint.h>
+
+//some global variables
+uint8_t     displaypage = 0;
+uint8_t     button_read = LOW;
+uint8_t     button_laststate = LOW;
+uint8_t     button_state = LOW;
+uint32_t    lastDebounceTime = 0;
+uint32_t    debounceDelay = 100;
+
+
+
+//////////////////////////////////////////functions
+
+void read_button() {
+
+  button_read = digitalRead(BUTTON);
+  //Serial.print(button_read);
+
+  if (button_read != button_laststate)  //pressed button
+  {
+    lastDebounceTime = millis();          //reset debouncing timer
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+
+    if (button_read != button_state)
+    {
+      button_state = button_read;
+
+      if (button_state == LOW)
+      {
+
+        displaypage++;
+        displaypage %= OLED_PANELS;
+
+      }
+    }
+
+  }
+  button_laststate = button_read;
+}
+
+/////////////////////// calculations
+
+void updateVars() {
+
+  float dstlon, dstlat;
+
+  //shrinking factor for longitude going to poles direction
+
+  float rads = fabs(uav_homelat) * 0.0174532925;
+  double scaleLongDown = cos(rads);
+  double scaleLongUp   = 1.0f / cos(rads);
+
+  if (uav_homefixstatus == 1) {
+
+    //DST to Home
+
+    dstlat = fabs(uav_homelat - uav_lat);
+    dstlon = fabs(uav_homelon - uav_lon) * scaleLongDown;
+    home_distance = sqrt(sq(dstlat) + sq(dstlon)) * 1.113195 / 100 ; // from cm to m
+
+    //DIR to Home
+
+    home_heading = (int) round (90 + (atan2(dstlat, -dstlon) * 57.295775)); //absolut home direction
+    if (home_heading < 0) home_heading += 360; //normalization
+    home_heading = home_heading - 180;//absolut return direction
+    if (home_heading < 0) home_heading += 360; //normalization
+  }
+  else
+  {
+    home_distance = 0;
+    home_heading = 0;
+  }
+
+}
+
+////////////////////////////////////// display OLED
+
+void display_oled() {
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+
+  switch (displaypage) {
+
+    case 0: //MAIN DATA SCREEN
+
+
+      display.print(F("Sats: ")); display.print(uav_satellites_visible);
+      display.print(F(" FS: "));  display.print(uav_failsafe);
+      display.print(F(" FM: "));  display.println(uav_flightmode);
+      display.print(F("ALT: "));  display.print(uav_alt); display.print(F(" SPD: "));  display.println(uav_groundspeed);
+      display.print(F("R: "));  display.print(uav_roll);
+      display.print(F(" P: "));  display.print(uav_pitch); display.print(F(" ARM: ")); display.println(uav_arm);
+      display.print(F("Lat: "));  display.println(uav_lat);
+      display.print(F("Lon: "));  display.println(uav_lon);
+      display.setTextSize(2);
+      display.print(F("RSSI: "));  display.println(uav_rssi);
+      display.setTextSize(1);
+      display.print(F("OK: "));  display.print(LTM_pkt_ok);
+      display.print(F(" KO: "));  display.println(LTM_pkt_ko);
+      break;
+
+    case 1:
+
+      display.setTextSize(2);
+      display.setCursor(0, 0);
+      display.print(F("RSSI:  "));  display.println(uav_rssi);
+      display.print(F("ALT:   "));  display.println(uav_alt);
+      display.print(F("VBATT: "));  display.println(uav_bat);
+      display.print(F("AMP:   "));  display.println(uav_amp);
+      display.setTextSize(1);
+      display.print(F("OK: "));  display.println(LTM_pkt_ok);
+      display.print(F(" KO: "));  display.println(LTM_pkt_ko);
+      break;
+
+    case 2:
+
+      display.print(F("HDOP: "));      display.println(uav_HDOP);
+      display.print(F("Lat : "));  display.println(uav_lat);
+      display.print(F("Lon : "));  display.println(uav_lon);
+      display.print(F("Home: ")); display.println(home_distance);
+      display.print(F("HDir: ")); display.println(home_heading);
+      display.print(F("HFix: ")); display.println(uav_homefixstatus);
+      display.print(F("HLat: ")); display.println(uav_homelat);
+      display.print(F("HLon: ")); display.println(uav_homelon);
+      break;
+    case 3:
+      display.setTextSize(2);
+      display.print(F("Home: ")); display.println(home_distance);
+      display.print(F("HDir: ")); display.println(home_heading);
+      display.print(F("RSSI: "));  display.println(uav_rssi);
+      display.print(F("ALT:  "));  display.println(uav_alt);
+      break;
+    case 4:
+
+      display.setTextSize(2);
+
+      display.print(F("Gmode: ")); display.println(uav_gpsmode);
+      display.print(F("Nmod:  ")); display.println(uav_navmode);
+      display.print(F("Nact:  "));  display.println(uav_navaction);
+      display.print(F("WPnr:  "));  display.println(uav_WPnumber);
+
+      break;
+    case 5:
+
+      display.setTextSize(2);
+
+//      display.print(F("FS:  ")); display.println(uav_failsafe);
+//      
+//      
+//      display.print(F("ARM: ")); display.println(uav_arm);
+//      display.print(F("Err: "));  display.println(ltm_naverror);
+      display.print(F("Flg: "));  display.println(ltm_flags);
+
+      break;
+
+    default:
+      break;
+
+
+  }
+
+  display.display();
+
+}
+
+//void blinkled() { //debug
+//
+//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(100);                       // wait for a second
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  delay(100);
+//
+//}
+
+/////////////////SETUP
+
+void setup() {
+
+  //Serial.begin(57600);        //debug port arduino
+  ltmSerial.begin(LTM_BAUDS);        //telemetry downlink is 9600. PLease set your telemetry downlink so.
+  //pinMode(LED_BUILTIN, OUTPUT); //debug
+  pinMode(BUTTON, INPUT);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //CRIUS CO-16 soldered pins to use adafruit libraries
+  display.setTextColor(WHITE);
+  display.clearDisplay();
+  //blinkled(); blinkled(); //debug
+
+}
+
+////////////////////////LOOP
+
+void loop() {
+
+  //while (ltmSerial.available()) {Serial.print(ltmSerial.read());}  //debug
+
+
+  ltm_read(); //read LTM telemetry
+
+  updateVars(); // calculate some variables from LTM data
+
+  read_button(); // check pushbutton and increase page counter
+
+  display_oled(); // display data in oled screen
+
+
+
+}
---
+++ LightTelemetry.h
@@ -0,0 +1,80 @@
+void ltm_read();
+void ltm_check();
+
+#define LIGHTTELEMETRY_START1 0x24 //$ HEADER '$'
+#define LIGHTTELEMETRY_START2 0x54 //T  HEADER 'T'
+#define LIGHTTELEMETRY_GFRAME 0x47 //G GPS + Baro altitude data ( Lat, Lon, Speed, Alt, Sats, Sat fix)
+#define LIGHTTELEMETRY_AFRAME 0x41 //A Attitude data ( Roll,Pitch, Heading )
+#define LIGHTTELEMETRY_SFRAME 0x53 //S Sensors/Status data ( VBat, Consumed current, Rssi, Airspeed, Arm status, Failsafe status, Flight mode )
+#define LIGHTTELEMETRY_OFRAME 0x4F //O  Origin data (home lon, home lat, homefix)
+#define LIGHTTELEMETRY_NFRAME 0x4E //N  NAVIGATION data
+#define LIGHTTELEMETRY_XFRAME 0x58 //X  EXTRA FRAME
+#define LIGHTTELEMETRY_GFRAMELENGTH 18
+#define LIGHTTELEMETRY_AFRAMELENGTH 10
+#define LIGHTTELEMETRY_SFRAMELENGTH 11
+#define LIGHTTELEMETRY_OFRAMELENGTH 18
+#define LIGHTTELEMETRY_NFRAMELENGTH 10
+#define LIGHTTELEMETRY_XFRAMELENGTH 10
+
+void telemetry_off(void);
+
+//Global variables
+int32_t       LTM_pkt_ko = 0;   //wrong LTM packet counter
+int32_t       LTM_pkt_ok = 0;   //good LTM packet counter
+int32_t       uav_homelat = 0; //home latitude
+int32_t       uav_homelon = 0; //home longitude
+int32_t       uav_homealt = 0; //home altitude
+uint8_t       uav_osd_on = 0; // allways 1
+uint32_t      home_distance = 0; // distante to home
+uint16_t      home_heading = 0; // heading to home
+uint8_t       uav_homefixstatus = 0; // 1=homefix ok
+uint8_t       uav_gpsmode = 0;
+uint8_t       uav_navmode = 0;
+uint8_t       uav_navaction = 0;
+uint8_t       uav_WPnumber = 0; // Waypoint number
+uint8_t       ltm_naverror = 0;
+uint8_t       ltm_flags = 0;
+uint16_t      uav_HDOP = 0; // GPS HDOP
+uint8_t       uav_HWstatus = 0; // hardware error
+uint8_t       uav_spare1 = 0;
+uint8_t       uav_spare2 = 0;
+uint8_t       ltm_spare3 = 0;
+
+int32_t      uav_lat = 0;                    // latitude
+int32_t      uav_lon = 0;                    // longitude
+float        lonScaleDown = 0.0;             // longitude scaling
+uint8_t      uav_satellites_visible = 0;     // number of satellites
+uint8_t      uav_fix_type = 0;               // GPS lock 0-1=no fix, 2=2D, 3=3D
+int32_t      uav_alt = 0;                    // altitude (dm)
+int32_t      rel_alt = 0;                    // relative altitude to home
+uint16_t     uav_groundspeed = 0;            // ground speed in km/h
+uint8_t      uav_groundspeedms = 0;          // ground speed in m/s
+int16_t      uav_pitch = 0;                  // attitude pitch
+int16_t      uav_roll = 0;                   // attitude roll
+int16_t      uav_heading = 0;                // attitude heading
+int16_t      uav_gpsheading = 0;             // gps heading
+uint16_t     uav_bat = 0;                    // battery voltage (mv)
+uint16_t     uav_amp = 0;                    // consumed mah.
+uint16_t     uav_current = 0;                // actual current
+uint8_t      uav_rssi = 0;                   // radio RSSI (%)
+uint8_t      uav_linkquality = 0;            // radio link quality
+uint8_t      uav_airspeed = 0;               // Airspeed sensor (m/s)
+uint8_t      ltm_armfsmode = 0;
+uint8_t      uav_arm = 0;                    // 0: disarmed, 1: armed
+uint8_t      uav_failsafe = 0;               // 0: normal,   1: failsafe
+uint8_t      uav_flightmode = 19;            // Flight mode(0-19): 0: Manual, 1: Rate, 2: Attitude/Angle, 3: Horizon, 4: Acro, 5: Stabilized1, 6: Stabilized2, 7: Stabilized3,
+// 8: Altitude Hold, 9: Loiter/GPS Hold, 10: Auto/Waypoints, 11: Heading Hold / headFree, 12: Circle, 13: RTH, 14: FollowMe, 15: LAND,
+// 16:FlybyWireA, 17: FlybywireB, 18: Cruise, 19: Unknown
+
+
+
+//long lastpacketreceived;
+
+
+
+boolean gps_fix      = false;
+//boolean btholdstate  = false;
+//boolean telemetry_ok = false;
+//boolean home_pos     = false;
+//boolean home_bear    = false;
+
