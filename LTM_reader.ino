f92533db93abf3dc7a17579eacca5d904a4644bf
sppnk <sppnk@users.noreply.github.com>
2017-03-29 16:52:41 +0200

    Update LTM_reader.ino

--- LTM_reader.ino
+++ LTM_reader.ino
@@ -264,11 +264,21 @@ void loop() {
   
   int32_t last_uav_lat = uav_lat; // last values of GPS for calculation of course over ground
   int32_t last_uav_lon = uav_lon;
-
+  uint16_t ground_courseRaw = 0;
+  static uint8_t sig = 0;
+  
   ltm_read();           //read LTM telemetry
   
-  GPS_dist_bearing(&uav_lat, &uav_lon, &last_uav_lat, &last_uav_lon, &ground_distance, &ground_course);  // distance between two GPS points (last and actual) and course of aircraft
-  //TODO average ground course over 2 secs.
+  // distance between two GPS points (last and actual) and course of aircraft
+  GPS_dist_bearing(&uav_lat, &uav_lon, &last_uav_lat, &last_uav_lon, &ground_distance, &ground_course); 
+  
+  //TODO average ground course 
+  
+    static uint16_t ground_courseRawArray[10];
+    ground_courseRawArray[(sig++)%10] = ground_course;
+    for (uint8_t i=0;i<10;i++) ground_courseRaw += ground_courseRawArray[i];
+    ground_course = ground_courseRaw / 10;  
+  
   //TODO measure millis() just to experiment calculating SPEED and comparing with GPS speed from LTM
   GPS_dist_bearing(&uav_lat, &uav_lon, &uav_homelat, &uav_homelon, &home_distance, &home_heading);        // calculate some variables from LTM data
 
