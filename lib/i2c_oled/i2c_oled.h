62abc3aafb798d724ad613bbcc92129aa79b5159
sppnk <sppnk@users.noreply.github.com>
2017-04-06 16:28:11 +0200

    Update i2c_oled.h

--- lib/i2c_oled/i2c_oled.h
+++ lib/i2c_oled/i2c_oled.h
@@ -1,1 +1,16 @@
 
+void i2c_OLED_send_cmd();
+void i2c_OLED_send_byte() ;
+void  i2c_OLED_init();
+void i2c_OLED_send_char();;
+void i2c_OLED_send_string();  
+void i2c_OLED_send_logo);
+void i2c_OLED_Put_Logo();
+void i2c_OLED_set_XY();       
+void i2c_OLED_set_line();  
+void i2c_clear_OLED();
+void i2c_writeReg();
+void waitTransmissionI2C();
+void i2c_stop();
+void i2c_write( );
+void i2c_rep_start();
